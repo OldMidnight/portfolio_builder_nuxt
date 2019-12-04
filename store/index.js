ff53c2f3f71a2dcf6f0228c0371d2d12248eb0c8
OldMidnight <fareedidris20@gmail.com>
2019-12-04 14:48:56 +0000

    fix nuxtserverinit

--- store/index.js
+++ store/index.js
@@ -1,36 +1,35 @@
 export const actions = {
   async nuxtServerInit({ commit }, { req, $axios }) {
-    const hosts = req.hostname.split('.')
-    console.log('HTRTYUKJGFTYJNB', req.hostname)
-    console.log('FSDFRESDEDFD', req)
-    let hostNum = 2
-    if (hosts.includes('herokuapp')) {
-      hostNum = 4
+    if (req.headers !== undefined) {
+      const hosts = req.hostname.split('.')
+      let hostNum = 2
+      if (hosts.includes('herokuapp')) {
+        hostNum = 4
+      }
+      if (req.hostname.split('.').length === hostNum) {
+        commit('creator/setIsSubdomain')
+        await $axios
+          .$post('/helper/check_domain', {
+            domain: req.hostname.split('.')[0]
+          })
+          .then((response) => {
+            const available = response.available
+            const active = response.active
+            if (available && active) {
+              commit('creator/setDomain', {
+                domain: { name: req.hostname.split('.')[0], available: true }
+              })
+            } else if (available && !active) {
+              commit('creator/setDomain', {
+                domain: { name: req.hostname.split('.')[0], available: false }
+              })
+            } else {
+              commit('creator/setDomain', {
+                domain: { name: null, available: false }
+              })
+            }
+          })
+      }
     }
-    if (req.hostname.split('.').length === hostNum) {
-      commit('creator/setIsSubdomain')
-      await $axios
-        .$post('/helper/check_domain', {
-          domain: req.hostname.split('.')[0]
-        })
-        .then((response) => {
-          const available = response.available
-          const active = response.active
-          if (available && active) {
-            commit('creator/setDomain', {
-              domain: { name: req.hostname.split('.')[0], available: true }
-            })
-          } else if (available && !active) {
-            commit('creator/setDomain', {
-              domain: { name: req.hostname.split('.')[0], available: false }
-            })
-          } else {
-            commit('creator/setDomain', {
-              domain: { name: null, available: false }
-            })
-          }
-        })
-    }
-    // console.log(route)
   }
 }
